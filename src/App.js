6daa231393b2743dbb324728b21fb439fdf91303
Robin Schmidt <schmidt.rbn@gmail.com>
2022-08-31 22:20:50 +0200

    modified Main.js, load movies via DataFetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from "./Components/Header";
 import Main from "./Components/Main";
-import DataFetch from "./Components/DataFetch";
+// import DataFetch from "./Components/DataFetch";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
+
 
       {/* <DataFetch /> */}
     </div>
