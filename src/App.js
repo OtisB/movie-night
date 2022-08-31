71db72397819fdd9ff3585a82bbb3d51bc0a874b
Rauolli <51509754+Rauolli@users.noreply.github.com>
2022-09-01 00:47:24 +0200

    added footer and a new folder img with images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Header from "./Components/Header";
 import DataFetch from "./Components/DataFetch";
+import Footer from "./Components/Footer";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <DataFetch />
+      <Footer />
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,46 @@
 import './Footer.css';
+import { NavLink } from 'react-router-dom';
+import Facebook from "../img/facebook.png";
+import Twitter from "../img/twitter.png";
+import LinkedIn from "../img/linkedin.png";
+import Instagram from "../img/instagram.png";
+import YouTube from "../img/youtube.png";
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+      <nav className="footer-navbar">
+        <ul className="nav-link-container">
+          <li>
+            <NavLink to="/inprint">Imprint</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/aboutus">About us</NavLink>
+          </li>
+          <li>
+            <NavLink to="/help">Help</NavLink>
+          </li>
+          <li>
+            <a href="https://m.facebook.com/" HTMLtarget="_blank"><img src={Facebook} alt="www.flaticon.com" width="30" /></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/" HTMLtarget="_blank"><img src={Twitter} alt="www.flaticon.com" width="30" /></a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/" HTMLtarget="_blank"><img src={LinkedIn} alt="www.flaticon.com" width="30" /></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/" HTMLtarget="_blank"><img src={Instagram} alt="www.flaticon.com" width="30" /></a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/" HTMLtarget="_blank"><img src={YouTube} alt="www.flaticon.com" width="30" /></a>
+          </li>
+          </ul>
+      </nav>
   )
 }
 
 export default Footer;
+
