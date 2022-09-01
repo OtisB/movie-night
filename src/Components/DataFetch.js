a80e72571591a217eae9bbf873c6a8090c49e21e
Niklas <Niklas-Riebesell@outlook.de>
2022-09-01 15:42:32 +0200

    refactor Fetch

--- src/Components/DataFetch.js
+++ src/Components/DataFetch.js
@@ -1,63 +1,23 @@
 import { client } from "../client";
 import { useEffect, useState } from "react";
 
-//rename to useDataFetch (best practice for custom hooks!)
-function DataFetch() {
+function useDataFetch() {
   const [Movies, setMovies] = useState([]);
 
-  const cleanUpData = (unCleanData) => {
-    const cleanData = unCleanData.map((field) => {
-      const { sys, fields } = field;
-      const { id } = sys;
-      const fieldTitle = fields.title;
-      const fieldImage = fields.image.fields.file.url;
-      const fieldDescription = fields.description;
-      const fieldRuntime = fields.runtime;
-      const fieldFsk = fields.fsk;
-      const fieldGenre = fields.genre;
-      const fieldDirectors = fields.directors;
-      const fieldCast = fields.cast;
-      const fieldScreenwriter = fields.screenwriter;
-      const fieldYearOfPuplication = fields.yearOfPuplication;
-      const fieldRating = fields.rating;
-      const fieldId = fields.id;
-      const updatedField = {
-        id,
-        fieldTitle,
-        fieldImage,
-        fieldDescription,
-        fieldRuntime,
-        fieldFsk,
-        fieldGenre,
-        fieldDirectors,
-        fieldCast,
-        fieldScreenwriter,
-        fieldYearOfPuplication,
-        fieldRating,
-        fieldId,
-      };
-      return updatedField;
-    });
-
-    setMovies(cleanData);
-  };
-
-  console.log("ich bin movies useState", Movies);
   useEffect(() => {
     getMovies();
   }, []);
 
   const getMovies = async () => {
     try {
-      const response = await client.getEntries({ content_type: "movieNight" });
-
+      const response = await client.getEntries();
       const responseData = response.items;
-      cleanUpData(responseData);
-
+      setMovies(responseData);
     } catch (error) {
       console.log(error);
     }
   };
+  console.log(Movies);
 
   // return (
   //   <div className="test">
@@ -84,13 +44,5 @@ function DataFetch() {
   // );
 
   //
-
-  console.log('return movies after fetch');
-  ///////// Code runs twice!!
-  return Movies;
-
-
-
 }
-
-export default DataFetch;
+export default useDataFetch;
