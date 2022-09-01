651284db891c766d779fd66c61e13dac014958ca
Niklas <Niklas-Riebesell@outlook.de>
2022-09-01 22:21:22 +0200

    added Login Site & basic Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import Login from "./Components/Login";
 // import useDataFetch from "./Components/useDataFetch";
 import Footer from "./Components/Footer";
-
-
 
 function App() {
   return (
@@ -13,11 +12,9 @@ function App() {
       {/* <useDataFetch /> */}
       <Footer />
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route
-          path="error"
-          element={<div>oops, something went wrong.</div>}
-        />
+        <Route path="/" element={<Main />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="error" element={<div>oops, something went wrong.</div>} />
         <Route path="*" element={<div>not found</div>} />
       </Routes>
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,34 @@
-import { NavLink } from 'react-router-dom';
-import './Header.css';
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   return (
     <div>
-      <nav className='navbar'>
+      <nav className="navbar">
         <div className="logo">
           {/* Placeholder Logo, replace with image */}
-          <NavLink to='/'>Logo</NavLink>
+          <NavLink to="/">Logo</NavLink>
         </div>
-        <ul className='nav-link-container'>
-          <NavLink to='/action'><li>Action</li></NavLink>
-          <NavLink to='/science-fiction'><li>Scifi</li></NavLink>
-          <NavLink to='/drama'><li>Drama</li></NavLink>
-          <NavLink to='/horror'><li>Horror</li></NavLink>
+        <ul className="nav-link-container">
+          <NavLink to="/action">
+            <li>Action</li>
+          </NavLink>
+          <NavLink to="/science-fiction">
+            <li>Scifi</li>
+          </NavLink>
+          <NavLink to="/drama">
+            <li>Drama</li>
+          </NavLink>
+          <NavLink to="/horror">
+            <li>Horror</li>
+          </NavLink>
         </ul>
         <div className="login">
-          <NavLink to='/login'>Login</NavLink>
+          <NavLink to="Login">Login</NavLink>
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
 export default Header;
