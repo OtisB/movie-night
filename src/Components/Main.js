61db8fbeefb2c438e0a9ffc498ad5ff03750bf89
Niklas <Niklas-Riebesell@outlook.de>
2022-09-01 16:21:05 +0200

    MovieOfTheDay

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,24 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import DataFetch from "./DataFetch";
-import './Main.css';
+import MovieOfTheDay from "./MovieOfTheDay";
+import "./Main.css";
 
 function Main() {
-
-  const movies = DataFetch();   //DataFetch is used as custom hook!
-  console.log('main');
+  const movies = DataFetch(); //DataFetch is used as custom hook!
+  console.log("main");
 
   const checkForMovies = () => {
-    return (!movies || movies.length === 0) ? false : true;
+    return !movies || movies.length === 0 ? false : true;
   };
 
   return (
-    <div className='main-container'>
-      {!checkForMovies() && <div className='no-data-message'>There are no Movies yet!</div>}
+    <div className="main-container">
+      {!checkForMovies() && (
+        <div className="no-data-message">There are no Movies yet!</div>
+      )}
       {checkForMovies() && <div>Yay, Movies!</div>}
       {/* {checkForMovies() && <Movie of the Day movies={movies}/>} */}
       {/* {checkForMovies() && <SmallCard movies={movies}/>} */}
+      <MovieOfTheDay />
     </div>
-  )
+  );
 }
 
 export default Main;
