973b0adcccd6e950cb64437023e2c8bf5aedd3e6
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-03 23:40:09 +0200

    Add: buttons navigate through movie cards added

--- src/Components/MovieCards.js
+++ src/Components/MovieCards.js
@@ -1,23 +1,54 @@
+import { useState, useEffect } from "react";
 import MovieCardSmall from "./MovieCardSmall";
 import "./MovieCards.css";
 
 function MovieCards({ movies }) {
+  const [firstMovieIndex, setFirstMovieIndex] = useState(0);
+  const [lastMovieIndex, setLastMovieIndex] = useState(4);
+  const [shownMovies, setShownMovies] = useState(movies.slice(firstMovieIndex, lastMovieIndex + 1));
 
-  let firstMovieIndex = 0;
-  let lastMovieIndex = 4;
-  // For Prev and next Buttons
   const shownRange = lastMovieIndex - firstMovieIndex;
 
-  const shownMovies = movies.slice(firstMovieIndex, lastMovieIndex + 1); // + 1 is cut off
+  const handleClickPrev = (event) => {
+    event.preventDefault();
+
+    if ((firstMovieIndex - shownRange) < 0) {
+      setFirstMovieIndex(0);
+      setLastMovieIndex(4);
+    } else {
+      setFirstMovieIndex(firstMovieIndex - shownRange - 1);
+      setLastMovieIndex(lastMovieIndex - shownRange - 1);
+    }
+  };
+
+  const handleClickNext = (event) => {
+    event.preventDefault();
+
+    if ((lastMovieIndex + shownRange) > movies.length) {
+      setFirstMovieIndex(((movies.length - 1)) - shownRange);
+      setLastMovieIndex(movies.length - 1);
+    } else {
+      setFirstMovieIndex(firstMovieIndex + shownRange + 1);
+      setLastMovieIndex(lastMovieIndex + shownRange + 1);
+    }
+  };
+
+  useEffect(() => {
+    setShownMovies(movies.slice(firstMovieIndex, lastMovieIndex + 1));
+  }, [firstMovieIndex, lastMovieIndex]);
 
   return (
-    <div className="cards-container">
-      {shownMovies.map(shownMovie => {
-        return (
-          <MovieCardSmall {...shownMovie} key={shownMovie.id} />
-        )
-      })}
-    </div>
+    <section id="cards">
+      <button onClick={handleClickPrev}>Prev</button>
+      <button onClick={handleClickNext}>Next</button>
+      <div className="cards-container">
+        {shownMovies.map(shownMovie => {
+          return (
+            <MovieCardSmall {...shownMovie} key={shownMovie.id} />
+          )
+        })}
+      </div>
+    </section>
   );
 }
 
