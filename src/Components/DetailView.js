6c1b476dda2eb14b3c0a7805ca8433f295d7e94d
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-05 18:36:08 +0200

    Modified: linked DetailView to MovieCardSmall

--- src/Components/DetailView.js
+++ src/Components/DetailView.js
@@ -1,26 +1,34 @@
+import { useParams } from "react-router-dom";
+import useDataFetch from "./useDataFetch";
 import './DetailView.css';
 import Rating from './Rating';
 
-function DetailView({randomMovie}){
-    console.log(randomMovie.fieldImage);
+function DetailView() {
 
-    return(
-        <article key={randomMovie.id} className="detail-view-container">
-            <figure>   
-                <figcaption><h2>{randomMovie.fieldTitle}</h2></figcaption>           
-                <img
-                    className="detail-view-image"
-                    src={randomMovie.fieldImage}
-                    alt={randomMovie.fieldTitle}
-                />
-            </figure>
+    const { id } = useParams();
+    const movies = useDataFetch();
+
+    const currentMovie = movies.find(movie => id == movie.id);
+
+    return (
+        <>
+            {currentMovie && <article key={currentMovie.id} className="detail-view-container">
+                <figure>
+                    <figcaption><h2>{currentMovie.fieldTitle}</h2></figcaption>
+                    <img
+                        className="detail-view-image"
+                        src={currentMovie.fieldImage}
+                        alt={currentMovie.fieldTitle}
+                    />
+                </figure>
                 <div className="detail-view-info">
-                    <p>Runtime: {randomMovie.fieldFsk}</p>
-                    <p>Rating: <Rating rating={randomMovie.fieldRating} /></p>
-                    <p>Runtime: {randomMovie.fieldRuntime}</p>
-                    <p>Production Year: {randomMovie.fieldYearOfPuplication}</p>
+                    <p>Runtime: {currentMovie.fieldFsk}</p>
+                    <p>Rating: <Rating rating={currentMovie.fieldRating} /></p>
+                    <p>Runtime: {currentMovie.fieldRuntime}</p>
+                    <p>Production Year: {currentMovie.fieldYearOfPuplication}</p>
                 </div>
-        </article>
+            </article>}
+        </>
     );
 }
 
