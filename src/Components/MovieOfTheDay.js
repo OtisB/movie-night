f85561f36a0ea168ffd44034cafef4322a72c515
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-05 18:43:16 +0200

    Modified: linked DetailView to MovieOfTheDay

--- src/Components/MovieOfTheDay.js
+++ src/Components/MovieOfTheDay.js
@@ -1,35 +1,37 @@
+import { Link } from 'react-router-dom';
 import "./MovieOfTheDay.css";
-import DetailView from './DetailView';
 
 function MovieOfTheDay({ movies }) {
   let randomMovie = movies[Math.floor(Math.random() * movies.length)];
   console.log(randomMovie);
 
   return (
-    <div key={randomMovie.id} className="MovieOfTheDayWrapper">
-      <div className="ImageAndTitle">
-        <img
-          className="MovieOfTheDayImage"
-          src={randomMovie.fieldImageQuer}
-          alt={randomMovie.fieldTitle}
-        />
-      </div>
-      <div className="SectionWrapper">
-        <h2>{randomMovie.fieldTitle}</h2>
-        <div className="InfoSectionWrapper">
-          <p>FSK: {randomMovie.fieldFsk}</p>{" "}
-          <ul className="RatingWrapper">
-            <li className="Rating isRated">&#9734;</li>
-            <li className="Rating isRated">&#9734;</li>
-            <li className="Rating isRated">&#9734;</li>
-            <li className="Rating isRated">&#9734;</li>
-            <li className="Rating isRated">&#9734;</li>
-          </ul>
-          <p>Runtime: {randomMovie.fieldRuntime} min</p>
-          <p>Production Year: {randomMovie.fieldYearOfPuplication}</p>
+    <Link to={`/${randomMovie.id}`}>
+      <div key={randomMovie.id} className="MovieOfTheDayWrapper">
+        <div className="ImageAndTitle">
+          <img
+            className="MovieOfTheDayImage"
+            src={randomMovie.fieldImageQuer}
+            alt={randomMovie.fieldTitle}
+          />
+        </div>
+        <div className="SectionWrapper">
+          <h2>{randomMovie.fieldTitle}</h2>
+          <div className="InfoSectionWrapper">
+            <p>FSK: {randomMovie.fieldFsk}</p>{" "}
+            <ul className="RatingWrapper">
+              <li className="Rating isRated">&#9734;</li>
+              <li className="Rating isRated">&#9734;</li>
+              <li className="Rating isRated">&#9734;</li>
+              <li className="Rating isRated">&#9734;</li>
+              <li className="Rating isRated">&#9734;</li>
+            </ul>
+            <p>Runtime: {randomMovie.fieldRuntime} min</p>
+            <p>Production Year: {randomMovie.fieldYearOfPuplication}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
     // <DetailView key={randomMovie.id} randomMovie={randomMovie} />
   );
 }
