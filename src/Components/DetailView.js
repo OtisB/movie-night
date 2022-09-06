15a658f42bf713b988acc20380a46c9359c37c5a
Rauolli <51509754+Rauolli@users.noreply.github.com>
2022-09-07 01:38:42 +0200

    bugfix for cast and directors in DetailView, and Style finished

--- src/Components/DetailView.js
+++ src/Components/DetailView.js
@@ -10,12 +10,24 @@ function DetailView() {
 
     const currentMovie = movies.find(movie => id == movie.id);
 
-    //console.log(currentMovie.fieldDirectors);
+    const listOf = (arr)=>{
+        let i = 0;
+        const arrLength = arr.length;
+        arr =  arr.map(item => {
+            if(i < arrLength){
+                item += ', ';
+            }
+            i++;
+            return item;
+        });
+
+        return arr;
+    };
+
     return (
         <>
             {currentMovie && <article key={currentMovie.id} className="detail-view-container">
                 <figure>
-                    <figcaption><h2>{currentMovie.fieldTitle}</h2></figcaption>
                     <img
                         className="detail-view-image"
                         src={currentMovie.fieldImageQuer}
@@ -23,12 +35,13 @@ function DetailView() {
                     />
                 </figure>
                 <div className="detail-view-info">
+                    <h2>{currentMovie.fieldTitle}</h2>
                     <p>FSK: {currentMovie.fieldFsk}</p>
                     <p>Rating: <Rating rating={currentMovie.fieldRating} /></p>
                     <p>Runtime: {currentMovie.fieldRuntime} Minutes</p>
                     <p>Production Year: {currentMovie.fieldYearOfPuplication}</p>
-                    <p>Directors: {currentMovie.fieldDirectors}</p>
-                    <p>Actors: {currentMovie.fieldCast}</p>
+                    <p>Directors: {listOf(currentMovie.fieldDirectors)}</p>
+                    <p>Actors: {listOf(currentMovie.fieldCast)}</p>
                     <p>Screenwriter: {currentMovie.fieldScreenwriter}</p>
                     <div className="description">
                         {currentMovie.fieldDescription}
