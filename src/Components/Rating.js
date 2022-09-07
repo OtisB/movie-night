9d6ba910569a132d4915ef3c70c16cbe7adfd628
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-07 19:45:10 +0200

    MODIFIED: ratings color

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,21 +1,21 @@
 import './Rating.css';
 
-function Rating({rating}){
+function Rating({ rating }) {
 
-    const ratings = ()=>{
-        const starFull = <span>&#9733;</span>;
+    const ratings = () => {
+        const starFull = <span className='star-full'>&#9733;</span>;
         const starEmpty = <span>&#9734;</span>;;
         let stars = [];
-        for (let i = 0; i < rating; i++){
-            stars.push(starFull);       
+        for (let i = 0; i < rating; i++) {
+            stars.push(starFull);
         }
 
-        for (let i = 0; i < (5-rating); i++) {
-            stars.push(starEmpty);           
+        for (let i = 0; i < (5 - rating); i++) {
+            stars.push(starEmpty);
         }
         return stars;
     }
-    return(
+    return (
         <span>{ratings()}</span>
     );
 }
